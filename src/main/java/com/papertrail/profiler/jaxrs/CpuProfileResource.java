466388b4f79ceab189e0f00a71cfc231f206b83a
phax <philip@helger.com>
2019-07-18 23:15:11 +0200

    Formatted

--- src/main/java/com/papertrail/profiler/jaxrs/CpuProfileResource.java
+++ src/main/java/com/papertrail/profiler/jaxrs/CpuProfileResource.java
@@ -1,54 +1,63 @@
 package com.papertrail.profiler.jaxrs;
 
-import com.papertrail.profiler.CpuProfile;
-import org.joda.time.Duration;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
-@Path("/pprof")
-public class CpuProfileResource {
-    final Lock lock = new ReentrantLock();
+import org.joda.time.Duration;
 
-    @Produces("pprof/raw")
-    @GET
-    @Path("profile")
-    public byte[] profile(
-            @QueryParam("duration") @DefaultValue("10") Integer duration,
-            @QueryParam("frequency") @DefaultValue("100") Integer frequency) throws IOException {
-        return doProfile(duration, frequency, Thread.State.RUNNABLE);
-    }
+import com.papertrail.profiler.CpuProfile;
 
-    @Produces("pprof/raw")
-    @GET
-    @Path("contention")
-    public byte[] contention(
-            @QueryParam("duration") @DefaultValue("10") Integer duration,
-            @QueryParam("frequency") @DefaultValue("100") Integer frequency) throws IOException {
-        return doProfile(duration, frequency, Thread.State.BLOCKED);
-    }
+@Path ("/pprof")
+public class CpuProfileResource
+{
+  final Lock lock = new ReentrantLock ();
 
-    protected byte[] doProfile(int duration, int frequency, Thread.State state) throws IOException {
-        if (lock.tryLock()) {
-            try {
-                CpuProfile profile = CpuProfile.record(Duration.standardSeconds(duration), frequency, state);
-                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                if (profile == null) {
-                    throw new RuntimeException("could not create CpuProfile");
-                }
-                profile.writeGoogleProfile(stream);
-                return stream.toByteArray();
-            } finally {
-                lock.unlock();
-            }
+  @Produces ("pprof/raw")
+  @GET
+  @Path ("profile")
+  public byte [] profile (@QueryParam ("duration") @DefaultValue ("10") final Integer duration,
+                          @QueryParam ("frequency") @DefaultValue ("100") final Integer frequency) throws IOException
+  {
+    return doProfile (duration.intValue (), frequency.intValue (), Thread.State.RUNNABLE);
+  }
+
+  @Produces ("pprof/raw")
+  @GET
+  @Path ("contention")
+  public byte [] contention (@QueryParam ("duration") @DefaultValue ("10") final Integer duration,
+                             @QueryParam ("frequency") @DefaultValue ("100") final Integer frequency) throws IOException
+  {
+    return doProfile (duration.intValue (), frequency.intValue (), Thread.State.BLOCKED);
+  }
+
+  protected byte [] doProfile (final int duration, final int frequency, final Thread.State state) throws IOException
+  {
+    if (lock.tryLock ())
+    {
+      try
+      {
+        final CpuProfile profile = CpuProfile.record (Duration.standardSeconds (duration), frequency, state);
+        final ByteArrayOutputStream stream = new ByteArrayOutputStream ();
+        if (profile == null)
+        {
+          throw new RuntimeException ("could not create CpuProfile");
         }
-        throw new RuntimeException("Only one profile request may be active at a time");
+        profile.writeGoogleProfile (stream);
+        return stream.toByteArray ();
+      }
+      finally
+      {
+        lock.unlock ();
+      }
     }
+    throw new RuntimeException ("Only one profile request may be active at a time");
+  }
 }
